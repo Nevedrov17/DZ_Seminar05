5514dc22cb59923620fd8f8b0fb6a30df828f682
NEVA <neva.vka@yandex.ru>
2022-10-15 23:29:03 +0300

    Задание 36, сумма нечетных

---
+++ Dz02/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] arr;
+arr = new int[size];
+int sum = 0;
+
+for( int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(0,100);
+    Console.Write(arr[i] + " ");
+}
+Console.WriteLine();
+
+for( int i = 0; i < arr.Length; i++)
+{
+    if(i % 2 > 0)
+    {
+        sum = sum + arr[i];
+    }
+}
+Console.WriteLine(sum);
