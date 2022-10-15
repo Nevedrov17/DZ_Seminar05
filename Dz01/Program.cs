e34430a62c1766ad9983931f73ae4c0e1784a783
NEVA <neva.vka@yandex.ru>
2022-10-15 23:15:04 +0300

    Задание 34, четные числа в массиве

--- Dz01/Program.cs
+++ Dz01/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿Console.Clear();
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] arr;
+arr = new int[size];
+int count = 0;
+
+for( int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(100,1000);
+    Console.Write(arr[i] + " ");
+}
+Console.WriteLine();
+
+for( int i = 0; i < arr.Length; i++)
+{
+    if(arr[i] % 2 == 0)
+    {
+       count++;
+    }
+}
+Console.WriteLine(count);
